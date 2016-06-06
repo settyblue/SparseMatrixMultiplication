130c8cc681a73959e3714e2edd8988dcb6e45bf6
Rakshith Kunchum <settyblue@gmail.com>
2016-06-06 11:23:00 +0100

    Modified script to test sparse matrix generation.

--- InitialAnalysis.py
+++ InitialAnalysis.py
@@ -1,7 +1,7 @@
 import scipy.sparse
 from scipy.sparse import diags
 import plotly
-import random
+import generateSpMM
 import numpy as np
 from numpy import random
 from numpy import linalg as LA
@@ -12,6 +12,7 @@ import plotly.graph_objs as go
 
 def build_rand_sparse_mat_and_multiply():
     sparse_matrix = scipy.sparse.rand(25,25,density=0.1)
+    #sparse_matrix = generateSpMM.generate_sparse_matrix(3,10,1)
     scipy.sparse.rand()
     array = sparse_matrix.toarray()
     array_as_list = array.tolist()
@@ -22,13 +23,22 @@ def build_rand_sparse_mat_and_multiply():
 
 def build_rand_sparse_diag_mat_and_multiply():
     array = diags(random.random_sample(25).tolist(), 0).toarray()
-    print array
+    #print array
     heat_map_data1 = [go.Heatmap(z=np.flipud(array).tolist())]
-    heat_map_data2 = [go.Heatmap(z=np.flipud((LA.matrix_power(array, 10))).tolist())]
+    heat_map_data2 = [go.Heatmap(z=np.flipud((LA.matrix_power(array, 2))).tolist())] #multiply with itself.
+    plot_url = plotly.offline.plot(heat_map_data1, filename='basic-heatmap1.html')
+    plot_url = plotly.offline.plot(heat_map_data2, filename='basic-heatmap2.html')
+
+def build_rand_sparse_mat_with_compression_ratio_and_multiply():
+    #array = diags(random.random_sample(25).tolist(), 0).toarray()
+    #print array
+    array = generateSpMM.generate_sparse_array(3,25,3)
+    heat_map_data1 = [go.Heatmap(z=np.flipud(array).tolist())]
+    heat_map_data2 = [go.Heatmap(z=np.flipud((LA.matrix_power(array, 2))).tolist())] #multiply with itself.
     plot_url = plotly.offline.plot(heat_map_data1, filename='basic-heatmap1.html')
     plot_url = plotly.offline.plot(heat_map_data2, filename='basic-heatmap2.html')
 
 def run():
-    build_rand_sparse_diag_mat_and_multiply()
+    build_rand_sparse_mat_with_compression_ratio_and_multiply()
 
 run()
